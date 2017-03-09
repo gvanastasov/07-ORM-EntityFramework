94bbb551eeb046268602432f8a005c45140e7640
gva <gvanastasov@abv.bg>
2017-03-10 00:33:50 +0100

    finished ex 12

--- 02_EF_Introduction/EF_Introduction/DB_First/App.cs
+++ 02_EF_Introduction/EF_Introduction/DB_First/App.cs
@@ -26,12 +26,72 @@ namespace DB_First
                     case "task 6": AddingNewAddressUpdatingEmployee(); break;
                     case "task 7": FindEmployeesInPeriod(); break;
                     case "task 8": AddressesByTownName(); break;
+                    case "task 9": EmployeeWithId(); break;
+                    case "task 10": DepartmentsWithMoreThan(); break;
+                    case "task 11": FindLatestTenProjects(); break;
+                    case "task 12": IncreaseSalaries(); break;
                     default:
                         Console.WriteLine("Use only 'task <int>' syntax to speciify task from homework. 'exit' command to return.");
                         break;
                 }
 
                 Console.WriteLine();
+            }
+        }
+
+        private static void IncreaseSalaries()
+        {
+            var targetDepartments = new string[] { "Engineering", "Tool Design", "Marketing", "Information Services" };
+            using (var ctx = new SoftuniContext())
+            {
+                var filtered = ctx.Employees.Where(e => targetDepartments.Contains(e.Department.Name)).ToList();
+                filtered.ForEach(emp =>
+                {
+                    emp.Salary += 0.12m * emp.Salary;
+                    Console.WriteLine($"{emp.FirstName} {emp.LastName} (${emp.Salary:F6})");
+                });
+
+                //ctx.SaveChanges();
+
+            }
+        }
+
+        private static void FindLatestTenProjects()
+        {
+            using (var ctx = new SoftuniContext())
+            {
+                var lastTen = ctx.Projects.OrderByDescending(p => p.StartDate).Take(10).OrderBy(p => p.Name).ToList();
+
+                foreach (var proj in lastTen)
+                {
+                    Console.WriteLine($"{proj.Name} {proj.Description} {proj.StartDate.ToString()} {proj.EndDate.ToString()}");
+                }
+            }
+        }
+
+        private static void DepartmentsWithMoreThan()
+        {
+            using (var ctx = new SoftuniContext())
+            {
+                var filtered = ctx.Departments.Where(d => d.Employees.Count > 5).OrderBy(d => d.Employees.Count).ToList();
+
+                foreach (var dep in filtered)
+                {
+                    Console.WriteLine($"{dep.Name} {ctx.Employees.FirstOrDefault(e =>e.EmployeeID == dep.ManagerID).FirstName}");
+                    dep.Employees.ToList().ForEach(e => Console.WriteLine($"{e.FirstName} {e.LastName} {e.JobTitle}"));
+                }
+            }
+        }
+
+        private static void EmployeeWithId()
+        {
+            using (var ctx = new SoftuniContext())
+            {
+                var e = ctx.Employees.FirstOrDefault(emp => emp.EmployeeID == 147);
+
+                Console.WriteLine($"{e.FirstName} {e.LastName} {e.JobTitle}");
+
+                e.Projects.OrderBy(p => p.Name).ToList().ForEach(p => Console.WriteLine($"{p.Name}"));
             }
         }
 
@@ -52,7 +112,7 @@ namespace DB_First
         {
             using (var ctx =  new SoftuniContext())
             {
-                var filtered = ctx.Employees.Where(e => e.Projects.Any(p => p.StartDate.Year > 2001 && p.StartDate.Year <= 2003)).ToList();
+                var filtered = ctx.Employees.Where(e => e.Projects.Any(p => p.StartDate.Year >= 2001 && p.StartDate.Year <= 2003)).Take(30).ToList();
 
                 foreach (var emp in filtered)
                 {
@@ -102,7 +162,7 @@ namespace DB_First
 
                 foreach (var e in filtered)
                 {
-                    Console.WriteLine($"{e.FirstName} {e.LastName} from {e.Department.Name} - ${e.Salary}");
+                    Console.WriteLine($"{e.FirstName} {e.LastName} from {e.Department.Name} - ${e.Salary:F2}");
                 }
             }
         }
